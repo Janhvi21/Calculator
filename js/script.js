fa0d1c310278cbbd2adc3211375d17dcf4aad6b9
Janhvi Chitnis <chitnisjanhvi1@gmail.com>
2021-03-04 14:06:31 -0500

    added comments and responsiveness

--- js/script.js
+++ js/script.js
@@ -14,42 +14,42 @@ const allClearOp = document.getElementById('data-all-clear');
 const equalOp = document.getElementById('data-equals');
 
 const calculator = new Calculator(previousOp, currentOp);
-calculatorArea.addEventListener("keyup", function (event) {
-    if (event.keyCode >= Key.ZERO && event.keyCode <= Key.NINE) {
+
+calculatorArea.addEventListener("keyup", function (event) {  //Event triggered on KeyBoard Number pressed
+    if (event.keyCode >= Key.ZERO && event.keyCode <= Key.NINE) {   //if numbers 0 - 9 pressed
         calculator.appendNumber(event.keyCode - 48);
         updateDisplay();
-    } else if (event.keyCode == Key.DOT) {
+    } else if (event.keyCode == Key.DOT) {   //if . pressed
         calculator.appendNumber('.');
         updateDisplay();
     }
-
 })
-numberButtons.forEach(button => {
+numberButtons.forEach(button => {   //Event on 1 - 9 button clicks
     button.addEventListener('click', (event) => {
         calculator.appendNumber(button.innerText)
         updateDisplay();
 
     })
 })
-operationButtons.forEach(button => {
+operationButtons.forEach(button => { //Event on operation / + * - clicks
     button.addEventListener('click', () => {
         calculator.chooseOperation(button.innerText)
         updateDisplay();
     })
 })
-equalOp.addEventListener('click', button => {
+equalOp.addEventListener('click', button => {  // Event on '=' click
     calculator.calculateResult()
     updateDisplay()
 })
-allClearOp.addEventListener('click', button => {
+allClearOp.addEventListener('click', button => { // Event triggered on 'AC' button click
     calculator.clearAll();
     updateDisplay();
 })
-deleteOp.addEventListener('click', button => {
+deleteOp.addEventListener('click', button => { // Event triggered on 'DEL' button click
     calculator.delete();
     updateDisplay();
 })
-
+// Update Display for results and operation inserted
 const updateDisplay = function () {
     calculator.currentOp.innerText = calculator.current;
     if (calculator.operation != null) {
